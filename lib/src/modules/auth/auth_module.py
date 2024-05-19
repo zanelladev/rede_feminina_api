c9af6773fd06bb0a8c852c2f06f8fd27991cb2d1
zanelladev <arturf.zanella@gmail.com>
2024-05-19 18:40:45 -0300

    chore: requirements

--- lib/src/modules/auth/auth_module.py
+++
@@ -1,8 +0,0 @@
-from injector import Module, singleton
-from lib.src.modules.auth.data.repositories.auth_repository import AuthRepository
-from lib.src.modules.auth.domain.repositories.i_auth_repository import IAuthRepository
-
-
-class AuthModule(Module):
-    def configure(self, binder):
-        binder.bind(IAuthRepository, to=AuthRepository, scope=singleton)
--- lib/src/modules/auth/domain/entities/user_entity.py
+++ lib/src/modules/auth/domain/entities/user_entity.py
@@ -1,9 +1,11 @@
 from lib.src.modules.auth.domain.entities.user_address import UserAddress
 
+
 class UserEntity:
     def __init__(self, userId: str, email: str, password: str, name: str, surname: str):
         if userId is None or email is None or password is None or name is None or surname is None:
-            raise ValueError("userId, email, password, name and surname are required.")
+            raise ValueError(
+                "userId, email, password, name and surname are required.")
         self.userId = userId
         self.email = email
         self.password = password
