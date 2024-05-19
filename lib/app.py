9ed95823461b641cd210488f33c9a4c78e558326
zanelladev <arturf.zanella@gmail.com>
2024-05-19 19:13:39 -0300

    refactor: app.py

--- lib/app.py
+++ lib/app.py
@@ -2,7 +2,7 @@ from lib.src.app_injections import AppInjections
 from lib.src.core.factories.flask.flask_factory import FlaskFactory
 
 AppInjections.registerBinds()
-app = FlaskFactory.create_app()
+app = FlaskFactory.init()
 
 if __name__ == "__main__":
     app.run()
