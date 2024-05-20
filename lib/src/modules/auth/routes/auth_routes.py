27f51a1f5b8d5079880bab4a25672e83a3d14357
zanelladev <arturf.zanella@gmail.com>
2024-05-19 22:43:32 -0300

    feat: sign up

--- lib/src/modules/auth/routes/auth_routes.py
+++ lib/src/modules/auth/routes/auth_routes.py
@@ -1,19 +1,26 @@
 from flask import request, jsonify, Blueprint
+from lib.src.core.services.injector.injector import Injector
 from lib.src.modules.auth.domain.entities.user_entity import UserEntity
+from lib.src.modules.auth.domain.repositories.i_auth_repository import IAuthRepository
 
 
 class AuthRoutes:
-    blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
-    users = [
-        UserEntity('id1', 'email1', 'password1', 'name1', 'surname1'),
-        UserEntity('id2', 'email2', 'password2', 'name2', 'surname2'),
-        UserEntity('id3', 'email3', 'password3', 'name3', 'surname3')
-    ]
+    blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
     @classmethod
     @blueprint.route("/login", methods=["POST"])
-    def login(cls):
+    def login():
+        auth_repository = Injector.retrieve(IAuthRepository)
+
+        auth_repository.signIn()
+
+        users = [
+            UserEntity('id1', 'email1', 'password1', 'name1', 'surname1'),
+            UserEntity('id2', 'email2', 'password2', 'name2', 'surname2'),
+            UserEntity('id3', 'email3', 'password3', 'name3', 'surname3')
+        ]
+
         if not request.is_json:
             return jsonify({"msg": "Missing JSON in request"}), 400
 
@@ -26,32 +33,23 @@ class AuthRoutes:
         if not password:
             return jsonify({"msg": "Missing password parameter"}), 400
 
-        for user in cls.users:
+        for user in users:
             if user.email == username and user.password == password:
                 return {"msg": "User login successfully."}, 200
 
         return {"msg": "User not found."}, 404
 
     @classmethod
-    @blueprint.route("/register", methods=["POST"])
-    def register(cls):
-        if not request.is_json:
-            return jsonify({"msg": "Missing JSON in request"}), 400
+    @blueprint.route("/signup", methods=["POST"])
+    async def register():
+        try:
+            auth_repository = Injector.retrieve(IAuthRepository)
+            await auth_repository.signUp()
 
-        username = request.json.get("email", None)
-        password = request.json.get("password", None)
+            return {"msg": "User not found."}, 200
 
-        if not username:
-            return jsonify({"msg": "Missing username parameter"}), 400
-
-        if not password:
-            return jsonify({"msg": "Missing password parameter"}), 400
-
-        for user in cls.users:
-            if user.email == username:
-                return {"msg": "User already exists."}, 400
-
-        new_user = UserEntity(str(len(cls.users) + 1), username, password)
-        cls.users.append(new_user)
-
-        return {"msg": "User registered successfully."}, 201
+        except Exception as e:
+            return jsonify({
+                "msg": "An error occurred while trying to sign up the user.",
+                "error": str(e)
+            }), 400
