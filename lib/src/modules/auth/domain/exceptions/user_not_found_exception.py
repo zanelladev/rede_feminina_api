553422d552be45fde77d1dcc93411bfc0edae57e
zanelladev <arturf.zanella@gmail.com>
2024-05-19 23:37:14 -0300

    feat: user not found exception

---
+++ lib/src/modules/auth/domain/exceptions/user_not_found_exception.py
@@ -0,0 +1,12 @@
+class UserNotFoundException(BaseException):
+    _message = "User not found"
+    _code = "USER_NOT_FOUND"
+
+    def __init__(self):
+        super().__init__(self._message, self._code)
+
+    def toJson(self):
+        return {
+            'message': self._message,
+            'code': self._code
+        }
