026fa4fe787c743e396a40030cfb3c72248b541d
zanelladev <arturf.zanella@gmail.com>
2024-05-21 23:55:05 -0300

    feat: dependency injection

--- lib/app.py
+++ lib/app.py
@@ -1,8 +1,9 @@
 from lib.src.app_injections import AppInjections
 from lib.src.core.factories.flask.flask_factory import FlaskFactory
+from lib.src.core.services.injector.injector import Injector
 
 AppInjections.registerBinds()
-app = FlaskFactory.instance()
+app = Injector.retrieve(FlaskFactory)
 
 if __name__ == "__main__":
     app.run()
--- lib/src/app_injections.py
+++ lib/src/app_injections.py
@@ -1,4 +1,5 @@
 from lib.src.core.factories.firebase.firebase_factory import FirebaseFactory
+from lib.src.core.factories.flask.flask_factory import FlaskFactory
 from lib.src.core.services.injector.injector import Injector
 from lib.src.modules.auth.data.repositories.auth_repository import AuthRepository
 from lib.src.modules.auth.domain.repositories.i_auth_repository import IAuthRepository
@@ -18,4 +19,8 @@ class AppInjections:
         Injector.register(
             AuthRoutes,
             AuthRoutes(Injector.retrieve(IAuthRepository)),
+        )
+        Injector.register(
+            FlaskFactory,
+            FlaskFactory.instance(Injector.retrieve(AuthRoutes)),
         )
